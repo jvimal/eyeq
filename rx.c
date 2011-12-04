d862f3d102186f1149ca6f5315c4c198e5d8da46
Vimalkumar <j.vimal@gmail.com>
2011-12-04 02:37:54 -0800

    rx path: classification, installation and processing
    The RX datapath still has work to do, but this is a good skeleton.

--- rx.c
+++ rx.c
@@ -1,5 +1,6 @@
 
 #include <linux/netfilter_bridge.h>
+#include "tx.h"
 #include "rx.h"
 #include "vq.h"
 
@@ -29,12 +30,115 @@ unsigned int iso_rx_bridge(unsigned int hooknum,
 			const struct net_device *out,
 			int (*okfn)(struct sk_buff *))
 {
+	struct iso_tx_class *txc;
+	iso_class_t klass;
+	struct iso_per_dest_state *state;
+	struct iso_vq *vq;
+
 	/* out will be NULL if this is PRE_ROUTING */
 	if(!in || strcmp(in->name, iso_param_dev) != 0)
 		return NF_ACCEPT;
 
+	rcu_read_lock();
+
+	/* Pick VQ */
+	klass = iso_rx_classify(skb);
+	vq = iso_vq_find(klass);
+	if(vq == NULL)
+		goto accept;
+
+	iso_vq_enqueue(vq, skb);
+
+	txc = iso_txc_find(klass);
+	if(txc == NULL)
+		goto accept;
+
+	state = iso_state_get(txc, skb);
+
+ accept:
+	rcu_read_unlock();
 	return NF_ACCEPT;
 }
+
+inline iso_class_t iso_rx_classify(struct sk_buff *skb) {
+	/* Classify just like TX context would have */
+	iso_class_t klass;
+#if defined ISO_TX_CLASS_DEV
+	klass = skb->dev;
+#elif defined ISO_TX_CLASS_ETHER_SRC
+	memcpy((void *)&klass, eth_hdr(skb)->h_dest, ETH_ALEN);
+#endif
+	return klass;
+}
+
+
+#if defined ISO_TX_CLASS_DEV
+int iso_vq_dev_install(char *name) {
+	struct iso_vq *vq;
+	struct net_device *dev;
+	int ret = 0;
+
+	rcu_read_lock();
+	dev = dev_get_by_name_rcu(&init_net, name);
+
+	if(dev == NULL) {
+		printk(KERN_INFO "perfiso: dev %s not found!\n", name);
+		ret = -1;
+		goto err;
+	}
+
+	/* Check if we have already created */
+	vq = iso_vq_find(dev);
+	if(vq != NULL) {
+		dev_put(dev);
+		ret = -1;
+		goto err;
+	}
+
+	vq = iso_vq_alloc(dev);
+
+	if(vq == NULL) {
+		dev_put(dev);
+		printk(KERN_INFO "perfiso: Could not allocate vq\n");
+		ret = -1;
+		goto err;
+	}
+
+ err:
+	rcu_read_unlock();
+	return ret;
+}
+#elif defined ISO_TX_CLASS_ETHER_SRC
+int iso_vq_ether_src_install(char *hwaddr) {
+	iso_class_t ether_src;
+	int ret = 0;
+	struct iso_vq *vq;
+
+	ret = -!mac_pton(hwaddr, (u8*)&ether_src);
+
+	if(ret) {
+		printk(KERN_INFO "perfiso: Cannot parse ether address from %s\n", hwaddr);
+		goto end;
+	}
+
+	/* Check if we have already created */
+	vq = iso_vq_find(ether_src);
+	if(vq != NULL) {
+		ret = -1;
+		goto end;
+	}
+
+	vq = iso_vq_alloc(ether_src);
+
+	if(vq == NULL) {
+		ret = -1;
+		goto end;
+	}
+
+ end:
+	return ret;
+}
+#endif
 
 /* Local Variables: */
 /* indent-tabs-mode:t */
--- rx.h
+++ rx.h
@@ -1,4 +1,5 @@
 
+#include "tx.h"
 
 int iso_rx_init(void);
 void iso_rx_exit(void);
@@ -7,6 +8,14 @@ unsigned int iso_rx_bridge(unsigned int hooknum,
 						   const struct net_device *in,
 						   const struct net_device *out,
 						   int (*okfn)(struct sk_buff *));
+
+inline iso_class_t iso_rx_classify(struct sk_buff *);
+
+#if defined ISO_TX_CLASS_DEV
+int iso_vq_dev_install(char *);
+#elif defined ISO_TX_CLASS_ETHER_SRC
+int iso_vq_ether_src_install(char *);
+#endif
 
 /* Local Variables: */
 /* indent-tabs-mode:t */
