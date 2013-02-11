61c9295af6a924384d46dfec0d893fa14a01d94d
Vimalkumar <j.vimal@gmail.com>
2013-02-11 11:10:00 -0800

    Update helper script to new config commands.

--- tools/create-ip-tenant.sh
+++ tools/create-ip-tenant.sh
@@ -2,9 +2,10 @@
 
 ip=$1
 dir=/sys/module/perfiso/parameters
+dev=eth2
 
-echo $ip > $dir/create_txc
-echo $ip > $dir/create_vq
-echo associate txc $ip vq $ip > $dir/assoc_txc_vq
-echo $ip weight 1 > $dir/set_vq_weight
+echo dev $dev $ip > $dir/create_txc
+echo dev $dev $ip > $dir/create_vq
+echo dev $dev associate txc $ip vq $ip > $dir/assoc_txc_vq
+echo dev $dev $ip weight 1 > $dir/set_vq_weight
 
